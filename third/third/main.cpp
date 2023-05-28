dc175f94bd3cdd1775c98c42407e53212299e451
Nick Malder <Nikita Baturin>
2023-05-28 18:24:19 +0300

    закончена 3я лабораторная

--- third/third/main.cpp
+++ third/third/main.cpp
@@ -1,136 +1,106 @@
 #include <iostream>
 #include <fstream>
+#include <complex>
 #include <cmath>
-#include <complex>
-#include <string>
 using namespace std;
-
-complex<int> Determ(complex<int>**matr, int n){
-    complex<int> det;
-    if(n == 1) det = matr[n-1][n-1];
-    if (n == 2)
-        det = matr[0][0]*matr[1][1] - matr[0][1]*matr[1][0];
-    if(n == 3){
-        det = matr[0][0]*matr[1][1]*matr[2][2] + matr[0][1]*matr[1][2]*matr[2][0] + matr[1][0]*matr[2][1]*matr[0][2];
-        det -= matr[2][0]*matr[1][1]*matr[0][2] - matr[1][0]*matr[0][1]*matr[2][2] - matr[2][1]*matr[1][2]*matr[0][0];
-    }
-    return det;
+int SLAU(complex <float>** matrica_a, int n, complex <float>* massiv_b, complex <float>* x){
+	int i, j, k, r;
+	complex <float> c, M, s;
+	float max;
+	complex <float>** a, * b;
+	a = new complex <float> *[n];
+	for (i = 0; i < n; i++)
+		a[i] = new complex <float>[n];
+	b = new complex <float>[n];
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			a[i][j] = matrica_a[i][j];
+	for (i = 0; i < n; i++)
+		b[i] = massiv_b[i];
+	for (k = 0; k < n; k++){
+		max = abs(a[k][k]);
+		r = k;
+		for (i = k + 1; i < n; i++)
+			if (abs(a[i][k]) > max){
+				max = abs(a[i][k]);
+				r = i;
+			}
+		for (j = 0; j < n; j++){
+			c = a[k][j];
+			a[k][j] = a[r][j];
+			a[r][j] = c;
+		}
+		c = b[k];
+		b[k] = b[r];
+		b[r] = c;
+		for (i = k + 1; i < n; i++){
+			for (M = a[i][k] / a[k][k], j = k; j < n; j++)
+				a[i][j] -= M * a[k][j];
+			b[i] -= M * b[k];
+		}
+	}
+	if (abs(a[n - 1][n - 1]) == 0)
+		if (abs(b[n - 1]) == 0)
+			return -1;
+		else return -2;
+	else{
+		for (i = n - 1; i >= 0; i--){
+			for (s = 0, j = i + 1; j < n; j++)
+				s += a[i][j] * x[j];
+			x[i] = (b[i] - s) / a[i][i];
+		}
+		return 0;
+	}
+	for (i = 0; i < n; i++)
+		delete[] a[i];
+	delete[] a;
+	delete[] b;
 }
-
-int main(){
-    ifstream size_matrix("matrix_A.txt"), matr_A("matrix_A.txt"), matr_B("matrix_B.txt");
-    if(size_matrix.is_open() && matr_A.is_open() && matr_B.is_open()){
-        int n = 0, buf;
-        string number;
-        char symbol;
-        
-        while(symbol != '\n'){
-            size_matrix >> number; n++;
-            size_matrix.get(symbol);
-        }
-        n /= 2;
-        size_matrix.close();
-
-        complex<int> **A = new complex<int>*[n], **B = new complex<int>*[n], **C_T = new complex<int>*[n], **C = new complex<int>*[n];
-        for(int i = 0; i < n; i++){
-            A[i] = new complex<int>[n];
-            B[i] = new complex<int>[n];
-            C_T[i] = new complex<int>[n];
-            C[i] = new complex<int>[n];
-            for(int j = 0; j < n; j++){
-                matr_A >> buf; A[i][j].real(buf);
-                matr_A >> buf; A[i][j].imag(buf);
-                matr_B >> buf; B[i][j].real(buf);
-                matr_B >> buf; B[i][j].imag(buf);
-            }
-        }
-        matr_A.close();
-        matr_B.close();
-        
-        cout << "A:\n";
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) cout << A[i][j] << " ";
-            cout << endl;
-        }
-
-        cout << "\nB:\n";
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) cout << B[i][j] << " "; 
-            cout << endl;
-        }
-/*Для заданных матриц комплексных чисел А(n×n) и B(n×n) найти C= ((A^2 +B^2 ))^T. Вычислить C^-1.*/
-        cout << "\nC:\n";
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                C_T[j][i] = A[i][j]*A[i][j] + B[i][j]*B[i][j];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) cout << C_T[i][j] << " ";
-            cout << endl;
-        }
-
-        cout << "\nC^-1:\n";
-        complex<int> det = Determ(C_T, n);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                C[i][j] = C_T[j][i] / det;
-                cout << C[i][j] << " ";
-            }
-            cout << endl;
-        }
-
-        for(int i = 0; i < n; i++){
-            delete[]A[i]; delete[]B[i];
-            delete[]C_T[i]; delete[]C[i];
-        }
-        delete[]A; delete[]B;
-        delete[]C_T; delete[]C;
-                
-    }        
-    return 0;
+int INVERSE(complex <float>** a, int n,complex <float>** y){
+	int i, j, res;
+	complex <float>* b, * x;
+	b = new complex <float>[n];
+	x = new complex <float>[n];
+	for (i = 0; i < n; i++){
+		for (j = 0; j < n; j++)
+			if (j == i)
+				b[j] = 1;
+			else b[j] = 0;
+		res = SLAU(a, n, b, x);
+		if (res != 0)
+			break;
+		else
+			for (j = 0; j < n; j++)
+				y[j][i] = x[j];
+	}
+	delete[] x;
+	delete[] b;
+	if (res != 0)
+		return -1;
+	else
+		return 0;
 }
-/*
-#include <iostream>
-#include <fstream>
-#include <complex>
-#include <ctime>
-using namespace std;
-//Для матриц А и В size*size найти С = (A^2 + B^2)^T
 int main() {
-    srand(time(NULL));
-    ofstream mat_A("matrix_A.txt"), mat_B("matrix_B.txt");
-    int sss = 3, p;
-    for (int i = 0; i < sss; i++) {
-        for (int j = 0; j < sss * 2; j++) {
-            p = 1 + rand() % 14;
-            mat_A << p << " ";
-            p = 1 + rand() % 14;
-            mat_B << p << " ";
-        }
-        mat_A << endl;
-        mat_B << endl;
-    }
-    mat_A.close(); mat_B.close();
-
     ifstream size_matrix("matrix_A.txt"), matr_A("matrix_A.txt"), matr_B("matrix_B.txt");
     if (size_matrix.is_open() && matr_A.is_open() && matr_B.is_open()) {
         int size = 0, buf;
         string number;
         char symbol;
-
-        while (symbol != '\size') {
+        while (symbol != '\n') {
             size_matrix >> number; size++;
             size_matrix.get(symbol);
         }
         size /= 2;
         size_matrix.close();
 
-        complex <int>** A = new complex<int>*[size], ** B = new complex<int>*[size], ** prom_C = new complex<int>*[size], ** C = new complex<int>*[size];
+        complex<float>** A = new complex<float> *[size], ** B = new complex<float> *[size], ** prom_C = new complex<float> *[size], ** C = new complex<float> *[size], ** reverse_C = new complex<float> *[size];
         for (int i = 0; i < size; i++) {
-            A[i] = new complex<int>[size];
-            B[i] = new complex<int>[size];
-            prom_C[i] = new complex<int>[size];
-            C[i] = new complex<int>[size];
-            for (int j = 0; j < size; j++) {
+            A[i] = new complex<float>[size];
+            B[i] = new complex<float>[size];
+            prom_C[i] = new complex<float>[size];
+            reverse_C[i] = new complex<float>[size];
+            C[i] = new complex<float>[size];
+            for (int j = 0; j < n; j++) {
                 matr_A >> buf; A[i][j].real(buf);
                 matr_A >> buf; A[i][j].imag(buf);
                 matr_B >> buf; B[i][j].real(buf);
@@ -139,40 +109,39 @@ int main() {
         }
         matr_A.close();
         matr_B.close();
-
-        cout << "\nA:\n";
+        cout << "A:\n";
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) cout << A[i][j] << " ";
             cout << endl;
         }
-
         cout << "\nB:\n";
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) cout << B[i][j] << " ";
             cout << endl;
         }
-
-        cout << "\nprom_C:\n";
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                A[i][j] *= A[i][j];
-                B[i][j] *= B[i][j];
-                prom_C[i][j] = A[i][j] + B[i][j];
-                cout << prom_C[i][j] << " ";
-            }
-            cout << endl;
-        }
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        cout << "\nC:\n";
+        for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++) {
+				A[i][j] *= A[i][j];
+				B[i][j] *= B[i][j];
+				prom_C[i][j] = A[i][j] + B[i][j];
+			}
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
                 C[j][i] = prom_C[i][j];
-            }
-        }
-
-        cout << "\nC^T:\n";
+        cout << "\nC:\n";
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) cout << C[i][j] << " ";
             cout << endl;
+        }
+
+        if (!INVERSE(C, size, reverse_C)) {
+            cout << endl << "Обратная матрица C:" << endl;
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++)
+                    cout << reverse_C[i][j] << " ";
+                cout << endl;
+            }
         }
 
         for (int i = 0; i < size; i++) {
@@ -180,9 +149,13 @@ int main() {
             delete[]B[i];
             delete[]C[i];
             delete[]prom_C[i];
+            delete[]reverse_C[i];
         }
-        delete[]A; delete[]B; delete[]C; delete[]prom_C;
+        delete[]A;
+        delete[]B;
+        delete[]C;
+        delete[]reverse_C;
+        delete[]prom_C;
     }
     return 0;
 }
-*/
